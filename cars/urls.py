470fb65eb4cae75c8f1528e2d542ae6514d4a787
Mohd Mujtaba <mujtaba.aamir1@gmail.com>
2020-05-12 10:25:07 +0530

    Added models and fixtures and migrations

---
+++ cars/urls.py
@@ -0,0 +1,6 @@
+from rest_framework.routers import SimpleRouter
+
+from cars import views
+
+
+app_name = 'cars'
--- django_elastic_search/settings.py
+++ django_elastic_search/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
-    'django_elasticsearch_dsl'
-    'django_elasticsearch_dsl_drf'
+    'rest_framework',
+    'django_elasticsearch_dsl',
+    'django_elasticsearch_dsl_drf',
     'cars',
 ]
 
